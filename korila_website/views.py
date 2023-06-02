3597ace3fd580e94e23a6ce0aee22c77ed9f2012
ReedikSober <114328546+ReedikSober@users.noreply.github.com>
2023-06-03 00:01:42 +0300

    Integrated base template, created corresponding html files, updated settings to accomodate static files, urls, views, integrate login and user authentication

--- korila_website/views.py
+++ korila_website/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def esileht(request):
+    return render(request, 'esileht.html')
 
 
-def contact(request):
-    return render(request, 'contact.html')
+def uldinfo(request):
+    return render(request, 'content_files/uldinfo.html')
+
+
+def seened(request):
+    return render(request, 'content_files/seened.html')
+
+
+def marjad(request):
+    return render(request, 'content_files/marjad.html')
+
+
+def meditsiinitaimed(request):
+    return render(request, 'content_files/meditsiinitaimed.html')
+
+
+def teetaimed(request):
+    return render(request, 'content_files/teetaimed.html')
+
+
+def varvitaimed(request):
+    return render(request, 'content_files/varvitaimed.html')
