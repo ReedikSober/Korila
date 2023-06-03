6edf3e00a1e3d98997bdcb746ab0ef431f78bc74
ReedikSober <114328546+ReedikSober@users.noreply.github.com>
2023-06-03 22:36:03 +0300

    Create inicial plant model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    months_to_harvest = models.PositiveIntegerField()
+    picture_url = models.URLField()
