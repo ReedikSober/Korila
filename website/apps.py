6b49bd8486328a4abf4375c814f38a0d5f672043
ReedikSober <114328546+ReedikSober@users.noreply.github.com>
2023-06-03 22:15:05 +0300

    Renamed project structure

--- website/apps.py
+++ website/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class KorilaWebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'korila_website'
+    name = 'website'
