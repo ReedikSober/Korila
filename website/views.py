c4ab332af6485836219d711a8513fda994cd3f00
ReedikSober <114328546+ReedikSober@users.noreply.github.com>
2023-06-07 09:35:32 +0300

    Select, submit and populate databease with selection of plants

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from website.models import SignUpForm, Flora
+from website.forms import UserSelectionForm
+from website.models import SignUpForm, Flora, UserSelection
 
 
 def esileht(request):
@@ -48,6 +49,21 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 
+@login_required
 def plant_list(request):
     plants = Flora.objects.all()
-    return render(request, 'kataloog.html', {'plants': plants})
+    user = request.user
+
+    if request.method == 'POST':
+        form = UserSelectionForm(request.POST)
+        if form.is_valid():
+            user_selection, created = UserSelection.objects.get_or_create(user=user)
+            user_selection.selected_plants.set(form.cleaned_data['selected_plants'])
+            messages.success(request, 'Selection saved successfully.')
+
+    else:
+        user_selection, created = UserSelection.objects.get_or_create(user=user)
+        initial_data = {'selected_plants': user_selection.selected_plants.all()}
+        form = UserSelectionForm(initial=initial_data)
+
+    return render(request, 'kataloog.html', {'plants': plants, 'form': form})
