4921e1251f826f471c89319df82522e7e0844b34
ReedikSober <114328546+ReedikSober@users.noreply.github.com>
2023-06-19 09:50:39 +0300

    Mayor update to calendar, the page now presents the flora objects according to harvest times and generates the box into the calendar.

--- calendarapp/templatetags/custom_filters.py
+++ calendarapp/templatetags/custom_filters.py
@@ -18,3 +18,38 @@ def multiply(value, arg):
 @register.filter
 def month_name(month_number):
     return datetime.strptime(str(month_number), "%m").strftime("%B")
+
+
+@register.simple_tag
+def evaluate_condition(index, start_month, start_week, end_month, end_week):
+    if (start_month - 1) * 4 + start_week <= index <= (end_month - 1) * 4 + end_week:
+        return 'true'
+    else:
+        return 'false'
+
+
+@register.simple_tag
+def before_range(start_month, start_week):
+    before_span = (start_month - 1) * 4 + start_week
+    return range(1, before_span)
+
+
+@register.simple_tag
+def flora_object_namespan(start_month, start_week, end_month, end_week):
+    before_span = (start_month - 1) * 4 + start_week
+    after_span = (end_month - 1) * 4 + end_week
+    result = int(before_span + (after_span - before_span) / 2)
+    return result
+
+
+@register.simple_tag
+def flora_object_range(start_month, start_week, end_month, end_week):
+    before_span = (start_month - 1) * 4 + start_week
+    after_span = (end_month - 1) * 4 + end_week + 1
+    return range(before_span, after_span)
+
+
+@register.simple_tag
+def after_range(end_month, end_week):
+    after_span = (end_month - 1) * 4 + end_week
+    return range(after_span, 48)
