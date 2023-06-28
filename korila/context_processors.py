1c2e764f516e5617c619e65c0ec0d1891357c161
ReedikSober <114328546+ReedikSober@users.noreply.github.com>
2023-06-28 11:51:51 +0300

    Added a "Welcome, User" on the banner

--- korila/context_processors.py
+++ korila/context_processors.py
@@ -1,4 +1,16 @@
+from django.contrib.auth.models import User
+
 def language_code(request):
     return {
         'language_code': request.LANGUAGE_CODE,
     }
+
+
+def user_name(request):
+    if request.user.is_authenticated:
+        user = request.user
+        user_name = user.username
+    else:
+        user_name = "Guest"
+
+    return {'user_name': user_name}
