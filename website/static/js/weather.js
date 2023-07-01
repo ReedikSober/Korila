cc993a04bb44cfc652694d289e989ae2529ce3e7
MarjeA <marjeavik@gmail.com>
2023-07-01 06:19:05 +0300

    moved weather widget on the navigation bar.

--- website/static/js/weather.js
+++ website/static/js/weather.js
@@ -1,73 +1,72 @@
 const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-fetch('https://api.tomorrow.io/v4/weather/forecast?location=estonia&timesteps=1d&units=metric&apikey=pwhMORMqNbY55DVHgvJ7p9ZSJxuc8Kak', options)
+window.addEventListener('load', () => {
+    fetch('https://api.tomorrow.io/v4/weather/forecast?location=estonia&timesteps=1d&units=metric&apikey=pwhMORMqNbY55DVHgvJ7p9ZSJxuc8Kak', options)
     .then(jsonData => jsonData.json())
-    .then(data => printIt(data))
-
-let printIt = (dataIn) => {
-    const data = dataIn;
-    const dayElementIdArray = ["day1", "day2", "day3"];
-    const avgTempElementIdArray = ["avg1", "avg2", "avg3"];
-    const avgHumElementIdArray = ["hum1", "hum2", "hum3"];
-    const minTempElementIdArray = ["min1", "min2", "min3"];
-
+    .then(data => {
+    function createAElement(innerText){
+        const para = document.createElement("a");
+        para.innerText = innerText;
+        const targetLi = document.getElementById("weatherList");
+        targetLi.appendChild(para);
+    }
+    function createBrElement(){
+        const para = document.createElement("br");
+        const targetLi = document.getElementById("weatherList");
+        targetLi.appendChild(para);
+    }
     function beforeDot(inputWithDot) {
         const stringWithDot = inputWithDot.toString()
         const justNumberArray = stringWithDot.split(".");
         let justNumber = justNumberArray[0];
         return justNumber;
     }
-
-    function dayNumberWithDot(elementId, whatDay) {
-        document.getElementById(elementId).innerHTML = data.timelines.daily[whatDay].time.slice(8, 10).concat(
+    function avargeTemp(whatDay) {
+        const avargeT = beforeDot(data.timelines.daily[whatDay].values.temperatureApparentAvg).concat("°C");
+        return avargeT;
+    }
+    function dayNumberWithDot(whatDay) {
+        const dayString = data.timelines.daily[whatDay].time.slice(8, 10).concat(
             ".", data.timelines.daily[whatDay].time.slice(5, 7));
+        return dayString;
     };
-
-    function avargeTemp(elementId, whatDay) {
-        document.getElementById(elementId).innerHTML = beforeDot(
-            data.timelines.daily[whatDay].values.temperatureApparentAvg).concat("°C");
+    function createADayArray(){
+        let dayArray = [];
+        for (let i = 0;i < 5;i++){
+            dayArray[i] = dayNumberWithDot(i);
+        }
+        return dayArray;
     }
-
-    function minimumTemp(elementId, whatday) {
-        document.getElementById(elementId).innerHTML = beforeDot(
-            data.timelines.daily[whatday].values.temperatureMin).concat("°C");
+    function createATempArray(){
+        let tempArrayv = [];
+        for (let i = 0; i < 5; i++){
+            tempArrayv[i] = avargeTemp(i);
+        }
+        return tempArrayv;
     }
-
-    function avargeHumidity(elementId, whatDay) {
-        document.getElementById(elementId).innerHTML = beforeDot(
-            data.timelines.daily[whatDay].values.humidityAvg).concat("%");
+    function setBGColorBasedOnTemp(temp){
+        const tempValue = temp;
+        let bgColor = "";
+        if (tempValue < 10) {
+            bgColor = "blue";
+        }else if (tempValue >= 10 && tempValue < 20) {
+            bgColor = "green";
+        } else if (tempValue >= 20 && tempValue < 30) {
+            bgColor = "yellow";
+        } else {
+            bgColor = "red";
+        }
+        console.log(bgColor);
+        document.body.style.Color = bgColor;
     }
-
-    function changeDayNumber(dayElementIdArray) {
-        const arrayLenght = dayElementIdArray.length;
-        for (let i = 0; i < arrayLenght; i++) {
-            dayNumberWithDot(dayElementIdArray[i], i);
+    function weatherForecast(array){
+        for (let i = 0; i < array.length; i++){
+            createAElement(array[i]);
         }
     }
 
-    function changeAvargeTemp(dayElementIdArray) {
-        const arrayLenght = dayElementIdArray.length;
-        for (let i = 0; i < arrayLenght; i++) {
-            avargeTemp(dayElementIdArray[i], i);
-        }
-    }
-
-    function changeAvargeHumidity(dayElementIdArray) {
-        const arrayLenght = dayElementIdArray.length;
-        for (let i = 0; i < arrayLenght; i++) {
-            avargeHumidity(dayElementIdArray[i], i);
-        }
-    }
-
-    function changeMinimumTemp(dayElementIdArray) {
-        const arrayLenght = dayElementIdArray.length;
-        for (let i = 0; i < arrayLenght; i++) {
-            minimumTemp(dayElementIdArray[i], i);
-        }
-    }
-
-    changeDayNumber(dayElementIdArray);
-    changeAvargeTemp(avgTempElementIdArray);
-    changeAvargeHumidity(avgHumElementIdArray);
-    changeMinimumTemp(minTempElementIdArray);
-};
+    weatherForecast(createADayArray());
+    createBrElement();
+    weatherForecast(createATempArray());
+    console.log(createADayArray());
+    })
+});
